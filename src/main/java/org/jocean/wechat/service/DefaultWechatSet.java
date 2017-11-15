10108445f297762f23bb1a86dffbb34dcee2b1e5
ming.ma <isdom.maming@gmail.com>
2017-11-15 17:55:20 +0800

    使用 EventBus.post 发布创建完成的 DefaultWechatSet 实例

--- src/main/java/org/jocean/wechat/service/DefaultWechatSet.java
+++ src/main/java/org/jocean/wechat/service/DefaultWechatSet.java
@@ -2,10 +2,18 @@ package org.jocean.wechat.service;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.jocean.j2se.eventbus.EventBusAware;
 import org.jocean.wechat.WechatAPI;
 import org.jocean.wechat.WechatSet;
 
-public class DefaultWechatSet extends ConcurrentHashMap<String, WechatAPI> implements WechatSet {
+import com.google.common.eventbus.EventBus;
+
+public class DefaultWechatSet extends ConcurrentHashMap<String, WechatAPI> implements WechatSet, EventBusAware {
 
     private static final long serialVersionUID = 1L;
+
+    @Override
+    public void setEventBus(final EventBus eventbus) {
+        eventbus.post(this);
+    }
 }
