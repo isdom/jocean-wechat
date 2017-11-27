b7d2f7963e712be553edf37593081445e8e64e92
ming.ma <isdom.maming@gmail.com>
2017-11-27 18:53:51 +0800

    改进 getAccessToken 实现

--- src/main/java/org/jocean/wechat/service/DefaultWXTokenSource.java
+++ src/main/java/org/jocean/wechat/service/DefaultWXTokenSource.java
@@ -16,7 +16,6 @@ import javax.inject.Inject;
 import org.jocean.http.Feature;
 import org.jocean.http.rosa.SignalClient;
 import org.jocean.idiom.ExceptionUtils;
-import org.jocean.idiom.Pair;
 import org.jocean.idiom.jmx.MBeanRegister;
 import org.jocean.idiom.jmx.MBeanRegisterAware;
 import org.jocean.wechat.WXTokenSource;
@@ -32,11 +31,8 @@ import com.google.common.collect.Lists;
 
 import io.netty.handler.ssl.SslContextBuilder;
 import rx.Observable;
-import rx.Observable.OnSubscribe;
 import rx.Subscriber;
 import rx.Subscription;
-import rx.functions.Action0;
-import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.subscriptions.Subscriptions;
 
@@ -45,6 +41,8 @@ import rx.subscriptions.Subscriptions;
  *
  */
 public class DefaultWXTokenSource implements WXTokenSource, MBeanRegisterAware {
+
+    private static final String MBEAN_SUFFIX = "info=wechat";
 
     private static final Logger LOG = 
             LoggerFactory.getLogger(DefaultWXTokenSource.class);
@@ -77,146 +75,75 @@ public class DefaultWXTokenSource implements WXTokenSource, MBeanRegisterAware {
      */
     @Override
     public Observable<String> getAccessToken(boolean forceRefresh) {
-        return Observable.unsafeCreate(new OnSubscribe<String>() {
-            @Override
-            public void call(final Subscriber<? super String> tokenSubscriber) {
-                doGetAccessToken(tokenSubscriber);
-            }});
-        
+        return Observable.unsafeCreate(subscriber -> {
+            if (!subscriber.isUnsubscribed()) {
+                while (!this._getTokenPolicy.call(subscriber))
+                    ;
+            }
+        });
     }
 
-    private void doGetAccessToken(final Subscriber<? super String> tokenSubscriber) {
-        this._tokenLock.readLock().lock();
-        try {
-            final Action1<Subscriber<? super String>> policy = this._getTokenPolicy;
-            policy.call(tokenSubscriber);
-        } finally {
-            this._tokenLock.readLock().unlock();
-        }
-    }
-    
-    private final Action1<Subscriber<? super String>> WAIT_FOR_UPDATE = 
-        new Action1<Subscriber<? super String>>() {
-            @Override
-            public void call(final Subscriber<? super String> subscriber) {
-                addToPendings(subscriber);
-            }};
-            
-    private final Action1<Subscriber<? super String>> USE_CURRENT = 
-        new Action1<Subscriber<? super String>>() {
-            @Override
-            public void call(final Subscriber<? super String> subscriber) {
-                pushCurrentToken(subscriber);
-            }};
-            
+    private final Func1<Subscriber<? super String>, Boolean> WAIT_FOR_UPDATE = subscriber->addToPendings(subscriber);
+    private final Func1<Subscriber<? super String>, Boolean> PUSH_TOKEN_NOW = subscriber->pushCurrentToken(subscriber);
+
     private void startToUpdateToken() throws Exception {
+        this._subscribers4accessToken = Lists.newCopyOnWriteArrayList();
         this._getTokenPolicy = WAIT_FOR_UPDATE;
-            
-        fetchAccessTokenFromWXOpenAPI()
-        .flatMap(new Func1<FetchAccessTokenResponse, 
-            Observable<Pair<FetchAccessTokenResponse, FetchTicketResponse>>>() {
-                @Override
-                public Observable<Pair<FetchAccessTokenResponse, FetchTicketResponse>> call(
-                        final FetchAccessTokenResponse tokenResp) {
-                    return fetchTicketFromWXOpenAPI(tokenResp.getAccessToken())
-                        .map(new Func1<FetchTicketResponse, Pair<FetchAccessTokenResponse, FetchTicketResponse>>() {
-                            @Override
-                            public Pair<FetchAccessTokenResponse, FetchTicketResponse> call(
-                                    final FetchTicketResponse ticketResp) {
-                                return Pair.of(tokenResp, ticketResp);
-                            }});
-                }})
-        .subscribe(new Action1<Pair<FetchAccessTokenResponse, FetchTicketResponse>>() {
-            @Override
-            public void call(final Pair<FetchAccessTokenResponse, FetchTicketResponse> tokenAndTicket) {
-                onAccessTokenResponse(tokenAndTicket.first);
-                updateMBean(tokenAndTicket.first.getAccessToken(), 
-                        tokenAndTicket.second.getTicket(),
-                        System.currentTimeMillis() + Long.parseLong(tokenAndTicket.first.getExpiresIn()) * 1000L);
-                scheduleUpdateToken(Long.parseLong(tokenAndTicket.first.getExpiresIn()) - 30, TimeUnit.SECONDS);
-            }}, new Action1<Throwable>() {
-            @Override
-            public void call(final Throwable error) {
+
+        fetchAccessTokenFromWXOpenAPI().doOnNext(resp -> onAccessTokenResponse(resp))
+            .flatMap(resp -> fetchTicketFromWXOpenAPI()).subscribe(resp -> {
+                updateMBean(resp.getTicket());
+                scheduleUpdateToken(Math.max(0L, this._accessTokenExpireInMs - System.currentTimeMillis()),
+                        TimeUnit.MILLISECONDS);
+            }, error -> {
                 onAccessTokenError(error);
                 // wait 10s, and retry
                 scheduleUpdateToken(10, TimeUnit.SECONDS);
-            }})
-        ;
+            });
     }
     
-    private Observable<FetchAccessTokenResponse> fetchAccessTokenFromWXOpenAPI() {
-        final FetchAccessTokenRequest fetchAccessTokenReq = new FetchAccessTokenRequest();
-        
-        fetchAccessTokenReq.setAppid(this._appid);
-        fetchAccessTokenReq.setSecret(this._secret);
-        try {
-            return this._signalClient.interaction().request(fetchAccessTokenReq)
-                .feature(Feature.ENABLE_LOGGING_OVER_SSL)
-                .feature(new Feature.ENABLE_SSL(SslContextBuilder.forClient().build()))
-                .feature(new SignalClient.UsingUri(new URI("https://api.weixin.qq.com/cgi-bin")))
-                .feature(new SignalClient.UsingPath("/token"))
-                .feature(new SignalClient.DecodeResponseBodyAs(FetchAccessTokenResponse.class))
-                .<FetchAccessTokenResponse>build()
-                .timeout(10, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            return Observable.error(e);
-        }
-    }
-
-    private Observable<FetchTicketResponse> fetchTicketFromWXOpenAPI(final String accessToken) {
-        final FetchTicketRequest fetchTicketRequest = new FetchTicketRequest();
-        
-        fetchTicketRequest.setAccessToken(accessToken);
-        fetchTicketRequest.setType("jsapi");
-        try {
-            return _signalClient.interaction().request(fetchTicketRequest)
-                .feature(Feature.ENABLE_LOGGING_OVER_SSL)
-                .feature(new Feature.ENABLE_SSL(SslContextBuilder.forClient().build()))
-                .feature(new SignalClient.UsingUri(new URI("https://api.weixin.qq.com/cgi-bin")))
-                .feature(new SignalClient.UsingPath("/ticket/getticket"))
-                .feature(new SignalClient.DecodeResponseBodyAs(FetchTicketResponse.class))
-                .<FetchTicketResponse>build()
-                .timeout(10, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            return Observable.error(e);
-        }
-    }
-    
-    private void scheduleUpdateToken(final long delayInSeconds, final TimeUnit unit) {
+    private void scheduleUpdateToken(final long delay, final TimeUnit unit) {
         if (this._isActive) {
-            this._timer = Observable.timer(delayInSeconds, unit)
-            .subscribe(new Action1<Long>() {
-                @Override
-                public void call(final Long unused) {
-                    _timer = null;
+            this._timer = Observable.timer(delay, unit)
+            .subscribe(unused -> {
+                    this._timer = null;
                     try {
                         startToUpdateToken();
                     } catch (Exception e) {
                         LOG.warn("exception when start to update, detail: {}",
                             ExceptionUtils.exception2detail(e));
                     }
-                }});
+                });
         } else {
             LOG.warn("wxtokensouce unactived, ignore scheduleUpdateToken.");
         }
     }
 
-    private void addToPendings(final Subscriber<? super String> subscriber) {
-        this._subscribers4accessToken.add(subscriber);
-        subscriber.add(Subscriptions.create(new Action0() {
-            @Override
-            public void call() {
-                _subscribers4accessToken.remove(subscriber);
-            }}));
+    private boolean addToPendings(final Subscriber<? super String> subscriber) {
+        this._subscribersLock.readLock().lock();
+        try {
+            final List<Subscriber<? super String>> subscribers = this._subscribers4accessToken;
+            if (null != subscribers) {
+                if (!subscriber.isUnsubscribed()) {
+                    subscribers.add(subscriber);
+                    subscriber.add(Subscriptions.create(() -> subscribers.remove(subscriber)));
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            this._subscribersLock.readLock().unlock();
+        }
     }
 
-    private void updateMBean(final String token,final String ticket, final long expireTime) {
+    private void updateMBean(final String ticket) {
         if (null!=this._register) {
             final String expireTimeAsString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                    .format(new Date(expireTime));
+                    .format(new Date(this._accessTokenExpireInMs));
             
-            this._register.unregisterMBean("info=wechat");
-            this._register.registerMBean("info=wechat", new WechatInfoMXBean() {
+            this._register.unregisterMBean(MBEAN_SUFFIX);
+            this._register.registerMBean(MBEAN_SUFFIX, new WechatInfoMXBean() {
                 
                 @Override
                 public String getName() {
@@ -225,7 +152,7 @@ public class DefaultWXTokenSource implements WXTokenSource, MBeanRegisterAware {
 
                 @Override
                 public String getAccessToken() {
-                    return token;
+                    return _accessToken;
                 }
 
                 @Override
@@ -250,47 +177,98 @@ public class DefaultWXTokenSource implements WXTokenSource, MBeanRegisterAware {
         }
     }
 
+    private Observable<FetchAccessTokenResponse> fetchAccessTokenFromWXOpenAPI() {
+        final FetchAccessTokenRequest fetchAccessTokenReq = new FetchAccessTokenRequest();
+        
+        fetchAccessTokenReq.setAppid(this._appid);
+        fetchAccessTokenReq.setSecret(this._secret);
+        try {
+            return this._signalClient.interaction().request(fetchAccessTokenReq)
+                .feature(Feature.ENABLE_LOGGING_OVER_SSL)
+                .feature(new Feature.ENABLE_SSL(SslContextBuilder.forClient().build()))
+                .feature(new SignalClient.UsingUri(new URI("https://api.weixin.qq.com/cgi-bin")))
+                .feature(new SignalClient.UsingPath("/token"))
+                .feature(new SignalClient.DecodeResponseBodyAs(FetchAccessTokenResponse.class))
+                .<FetchAccessTokenResponse>build()
+                .timeout(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            return Observable.error(e);
+        }
+    }
+
+    private Observable<FetchTicketResponse> fetchTicketFromWXOpenAPI() {
+        final FetchTicketRequest fetchTicketRequest = new FetchTicketRequest();
+        
+        fetchTicketRequest.setAccessToken(this._accessToken);
+        fetchTicketRequest.setType("jsapi");
+        try {
+            return this._signalClient.interaction().request(fetchTicketRequest)
+                .feature(Feature.ENABLE_LOGGING_OVER_SSL)
+                .feature(new Feature.ENABLE_SSL(SslContextBuilder.forClient().build()))
+                .feature(new SignalClient.UsingUri(new URI("https://api.weixin.qq.com/cgi-bin")))
+                .feature(new SignalClient.UsingPath("/ticket/getticket"))
+                .feature(new SignalClient.DecodeResponseBodyAs(FetchTicketResponse.class))
+                .<FetchTicketResponse>build()
+                .timeout(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            return Observable.error(e);
+        }
+    }
+    
     private void onAccessTokenResponse(final FetchAccessTokenResponse resp) {
         this._accessToken = resp.getAccessToken();
-        this._accessTokenExpireInMs = System.currentTimeMillis() 
-                + (Long.parseLong(resp.getExpiresIn()) - 30) * 1000L;
+        this._accessTokenExpireInMs = System.currentTimeMillis() + (Long.parseLong(resp.getExpiresIn()) - 30) * 1000L;
         LOG.info("on fetch access token response {}, update token {} and expires timestamp in {}", 
                 resp, _accessToken, new Date(_accessTokenExpireInMs));
         
-        this._tokenLock.writeLock().lock();
-        try {
-            this._getTokenPolicy = USE_CURRENT;
-            for (Subscriber<? super String> subscriber : this._subscribers4accessToken) {
+        this._getTokenPolicy = PUSH_TOKEN_NOW;
+        
+        final List<Subscriber<? super String>> subscribers = this._subscribers4accessToken;
+        
+        this._subscribersLock.writeLock().lock();
+        this._subscribers4accessToken = null;
+        this._subscribersLock.writeLock().unlock();
+        
+        if (null != subscribers) {
+            for (Subscriber<? super String> subscriber : subscribers) {
                 pushCurrentToken(subscriber);
             }
-            this._subscribers4accessToken.clear();
-        } finally {
-            this._tokenLock.writeLock().unlock();
+            subscribers.clear();
         }
     }
 
     private void onAccessTokenError(final Throwable error) {
-        this._tokenLock.writeLock().lock();
-        try {
-            for (Subscriber<? super String> subscriber : this._subscribers4accessToken) {
+        this._getTokenPolicy = PUSH_TOKEN_NOW;
+        
+        final List<Subscriber<? super String>> subscribers = this._subscribers4accessToken;
+        
+        this._subscribersLock.writeLock().lock();
+        this._subscribers4accessToken = null;
+        this._subscribersLock.writeLock().unlock();
+        
+        if (null != subscribers) {
+            for (Subscriber<? super String> subscriber : subscribers) {
                 pushCurrentTokenWithError(subscriber, error);
             }
-            this._subscribers4accessToken.clear();
-        } finally {
-            this._tokenLock.writeLock().unlock();
+            subscribers.clear();
         }
     }
 
-    private void pushCurrentToken(final Subscriber<? super String> subscriber) {
+    private boolean pushCurrentToken(final Subscriber<? super String> subscriber) {
         if (!subscriber.isUnsubscribed()) {
             try {
-                subscriber.onNext(this._accessToken);
-                subscriber.onCompleted();
+                if (null != this._accessToken) {
+                    subscriber.onNext(this._accessToken);
+                    subscriber.onCompleted();
+                } else {
+                    subscriber.onError(new RuntimeException("access token is null"));
+                }
             } catch (Exception e) {
                 LOG.warn("exception when invoke {}.onNext, detail: {}",
                         subscriber, ExceptionUtils.exception2detail(e));
             }
         }
+        return true;
     }
 
     private void pushCurrentTokenWithError(final Subscriber<? super String> subscriber, 
@@ -318,12 +296,10 @@ public class DefaultWXTokenSource implements WXTokenSource, MBeanRegisterAware {
     
     private long _accessTokenExpireInMs = 0;
     
-    private final List<Subscriber<? super String>> _subscribers4accessToken = 
-            Lists.newCopyOnWriteArrayList();
+    private final ReadWriteLock _subscribersLock = new ReentrantReadWriteLock(false);
+    private List<Subscriber<? super String>> _subscribers4accessToken;
     
-    private volatile Action1<Subscriber<? super String>> _getTokenPolicy = WAIT_FOR_UPDATE;
-    
-    private ReadWriteLock _tokenLock = new ReentrantReadWriteLock(false);
+    private volatile Func1<Subscriber<? super String>, Boolean> _getTokenPolicy;
     
     @Inject
     private SignalClient _signalClient;
