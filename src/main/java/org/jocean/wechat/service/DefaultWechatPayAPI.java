99f92e707a82cb96ab82652e712c5eae6214271c
ming.ma <isdom.maming@gmail.com>
2018-02-22 17:03:18 +0800

    尝试 setFlushPerWrite true

--- src/main/java/org/jocean/wechat/service/DefaultWechatPayAPI.java
+++ src/main/java/org/jocean/wechat/service/DefaultWechatPayAPI.java
@@ -107,8 +107,10 @@ public class DefaultWechatPayAPI implements WechatPayAPI, MBeanRegisterAware {
                             .reqbean(reqAndBody)
                             .body(MessageUtil.toBody(reqAndBody, MediaType.APPLICATION_XML, MessageUtil::serializeToXml))
                             .feature(Feature.ENABLE_LOGGING_OVER_SSL, new Feature.ENABLE_SSL(sslctx)).execution())
-                            .compose(MessageUtil.responseAs(SendRedpackResponse.class, MessageUtil::unserializeAsXml))
-                            .retryWhen(retryPolicy()).map(resp -> Proxys.delegate(SendRedpackResult.class, resp));
+                        // TODO try setFlushPerWrite true
+                        .doOnNext(interaction -> interaction.initiator().writeCtrl().setFlushPerWrite(true))
+                        .compose(MessageUtil.responseAs(SendRedpackResponse.class, MessageUtil::unserializeAsXml))
+                        .retryWhen(retryPolicy()).map(resp -> Proxys.delegate(SendRedpackResult.class, resp));
                 } catch (Exception e) {
                     LOG.warn("exception when sendRedpack {}, detail: {}", reqAndBody, ExceptionUtils.exception2detail(e));
                     return Observable.error(e);
