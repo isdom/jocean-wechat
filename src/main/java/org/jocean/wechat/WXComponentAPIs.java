c5f856cd12e74e18fe7d50b7cf7b140d16523ce2
ming.ma <isdom.maming@gmail.com>
2018-03-17 21:40:44 +0800

    change return type to Func1<Interact, Observable<XXX>>

--- src/main/java/org/jocean/wechat/WXComponentAPIs.java
+++ src/main/java/org/jocean/wechat/WXComponentAPIs.java
@@ -2,12 +2,13 @@
 package org.jocean.wechat;
 
 import org.jocean.http.Feature;
+import org.jocean.http.Interact;
 import org.jocean.http.MessageUtil;
-import org.jocean.http.client.HttpClient;
 import org.jocean.wechat.spi.OAuthAccessTokenResponse;
 import org.jocean.wechat.spi.UserInfoResponse;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 public class WXComponentAPIs {
     private WXComponentAPIs() {
@@ -15,34 +16,32 @@ public class WXComponentAPIs {
     }
     
     
-    public static Observable<OAuthAccessTokenResponse> getOAuthAccessToken(
-            final HttpClient client,
+    public static Func1<Interact, Observable<OAuthAccessTokenResponse>> getOAuthAccessToken(
             final String appid, 
             final String componentAppid, 
             final String componentAccessToken,
             final String code) {
-        return MessageUtil.interact(client)
-            .uri("https://api.weixin.qq.com")
-            .path("/sns/oauth2/component/access_token")
-            .paramAsQuery("appid", appid)
-            .paramAsQuery("code", code)
-            .paramAsQuery("grant_type", "authorization_code")
-            .paramAsQuery("component_appid", componentAppid)
-            .paramAsQuery("component_access_token", componentAccessToken)
-            .feature(Feature.ENABLE_LOGGING_OVER_SSL).execution()
-            .compose(MessageUtil.responseAs(OAuthAccessTokenResponse.class, MessageUtil::unserializeAsJson));
+        return interact->interact
+                .uri("https://api.weixin.qq.com")
+                .path("/sns/oauth2/component/access_token")
+                .paramAsQuery("appid", appid)
+                .paramAsQuery("code", code)
+                .paramAsQuery("grant_type", "authorization_code")
+                .paramAsQuery("component_appid", componentAppid)
+                .paramAsQuery("component_access_token", componentAccessToken)
+                .feature(Feature.ENABLE_LOGGING_OVER_SSL).execution()
+                .compose(MessageUtil.responseAs(OAuthAccessTokenResponse.class, MessageUtil::unserializeAsJson));
     }
 
-    public static Observable<UserInfoResponse> getSnsapiUserInfo(
-            final HttpClient client,
+    public static Func1<Interact, Observable<UserInfoResponse>> getSnsapiUserInfo(
             final String snsapiToken, 
             final String openid) {
-        return MessageUtil.interact(client)
-            .feature(Feature.ENABLE_LOGGING_OVER_SSL)
-            .uri("https://api.weixin.qq.com").path("/sns/userinfo")
-            .paramAsQuery("access_token", snsapiToken).paramAsQuery("openid", openid)
-            .paramAsQuery("lang", "zh_CN")
-            .execution()
-            .compose(MessageUtil.responseAs(UserInfoResponse.class, MessageUtil::unserializeAsJson));
+        return interact->interact
+                .feature(Feature.ENABLE_LOGGING_OVER_SSL)
+                .uri("https://api.weixin.qq.com").path("/sns/userinfo")
+                .paramAsQuery("access_token", snsapiToken).paramAsQuery("openid", openid)
+                .paramAsQuery("lang", "zh_CN")
+                .execution()
+                .compose(MessageUtil.responseAs(UserInfoResponse.class, MessageUtil::unserializeAsJson));
     }
 }
