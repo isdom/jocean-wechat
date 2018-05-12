37aa3667f277d8f76b910eab07cf7111e532817a
ming.ma <isdom.maming@gmail.com>
2018-05-12 17:18:17 +0800

    移除重复实现: WXComponentAPIs.getSnsapiUserInfo

--- src/main/java/org/jocean/wechat/WXComponentAPIs.java
+++ src/main/java/org/jocean/wechat/WXComponentAPIs.java
@@ -5,7 +5,6 @@ import org.jocean.http.Feature;
 import org.jocean.http.Interact;
 import org.jocean.http.MessageUtil;
 import org.jocean.wechat.WXProtocol.OAuthAccessTokenResponse;
-import org.jocean.wechat.WXProtocol.UserInfoResponse;
 
 import rx.Observable;
 import rx.functions.Func1;
@@ -31,18 +30,5 @@ public class WXComponentAPIs {
                 .paramAsQuery("component_access_token", componentAccessToken)
                 .feature(Feature.ENABLE_LOGGING_OVER_SSL).execution()
                 .compose(MessageUtil.responseAs(OAuthAccessTokenResponse.class, MessageUtil::unserializeAsJson));
-    }
-
-    public static Func1<Interact, Observable<UserInfoResponse>> getSnsapiUserInfo(
-            final String snsapiToken,
-            final String openid) {
-        return interact->interact
-                .feature(Feature.ENABLE_LOGGING_OVER_SSL)
-                .uri("https://api.weixin.qq.com").path("/sns/userinfo")
-                .paramAsQuery("access_token", snsapiToken)
-                .paramAsQuery("openid", openid)
-                .paramAsQuery("lang", "zh_CN")
-                .execution()
-                .compose(MessageUtil.responseAs(UserInfoResponse.class, MessageUtil::unserializeAsJson));
     }
 }
