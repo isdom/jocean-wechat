eec00777d0474b21863f5a9040ca4035875a4e3a
ming.ma <isdom@qq.com>
2018-08-22 11:51:13 +0200

    改进 Getter / Setter 命名

--- src/main/java/org/jocean/wechat/spi/PayBaseResponse.java
+++ src/main/java/org/jocean/wechat/spi/PayBaseResponse.java
@@ -1,6 +1,108 @@
 package org.jocean.wechat.spi;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
 public class PayBaseResponse {
+    @JacksonXmlProperty(localName="return_code")
+    public String getReturnCode() {
+        return return_code;
+    }
+
+    @JacksonXmlProperty(localName="return_code")
+    public void setReturnCode(final String return_code) {
+        this.return_code = return_code;
+    }
+
+    @JacksonXmlProperty(localName="return_msg")
+    public String getReturnMsg() {
+        return return_msg;
+    }
+
+    @JacksonXmlProperty(localName="return_msg")
+    public void setReturnMsg(final String return_msg) {
+        this.return_msg = return_msg;
+    }
+
+    @JacksonXmlProperty(localName="appid")
+    public String getAppid() {
+        return appid;
+    }
+
+    @JacksonXmlProperty(localName="appid")
+    public void setAppid(final String appid) {
+        this.appid = appid;
+    }
+
+    @JacksonXmlProperty(localName="mch_id")
+    public String getMchId() {
+        return mch_id;
+    }
+
+    @JacksonXmlProperty(localName="mch_id")
+    public void setMchId(final String mch_id) {
+        this.mch_id = mch_id;
+    }
+
+    @JacksonXmlProperty(localName="device_info")
+    public String getDeviceInfo() {
+        return device_info;
+    }
+
+    @JacksonXmlProperty(localName="device_info")
+    public void setDeviceInfo(final String device_info) {
+        this.device_info = device_info;
+    }
+
+    @JacksonXmlProperty(localName="nonce_str")
+    public String getNonceStr() {
+        return nonce_str;
+    }
+
+    @JacksonXmlProperty(localName="nonce_str")
+    public void setNonceStr(final String nonce_str) {
+        this.nonce_str = nonce_str;
+    }
+
+    @JacksonXmlProperty(localName="sign")
+    public String getSign() {
+        return sign;
+    }
+
+    @JacksonXmlProperty(localName="sign")
+    public void setSign(final String sign) {
+        this.sign = sign;
+    }
+
+    @JacksonXmlProperty(localName="result_code")
+    public String getResultCode() {
+        return result_code;
+    }
+
+    @JacksonXmlProperty(localName="result_code")
+    public void setResultCode(final String result_code) {
+        this.result_code = result_code;
+    }
+
+    @JacksonXmlProperty(localName="err_code")
+    public String getErrCode() {
+        return err_code;
+    }
+
+    @JacksonXmlProperty(localName="err_code")
+    public void setErrCode(final String err_code) {
+        this.err_code = err_code;
+    }
+
+    @JacksonXmlProperty(localName="err_code_des")
+    public String getErrCodeDes() {
+        return err_code_des;
+    }
+
+    @JacksonXmlProperty(localName="err_code_des")
+    public void setErrCodeDes(final String err_code_des) {
+        this.err_code_des = err_code_des;
+    }
+
     //协议层
     protected String return_code; //SUCCESS/FAIL
     protected String return_msg;
@@ -9,7 +111,7 @@ public class PayBaseResponse {
     //  小程序ID    appid   是   String(32)  wxd678efh567hg6787  微信分配的小程序ID
     protected String appid;
     // 商户号  mch_id  是   String(32)  1230000109  微信支付分配的商户号
-    protected String mch_id;//	String(32) 	1900000109 	微信支付分配的商户号
+    protected String mch_id;//  String(32)  1900000109  微信支付分配的商户号
 
     // 随机字符串    nonce_str   是   String(32)  5K8264ILTKCH16CQ2502SI8ZNMTM67VS    随机字符串，不长于32位。推荐随机数生成算法
     protected String nonce_str;
@@ -30,84 +132,4 @@ public class PayBaseResponse {
     // 如trade_state不为 SUCCESS，则只返回out_trade_no（必传）和attach（选传）。
     // 设备号  device_info 否   String(32)  013467007045764 微信支付分配的终端设备号，
     protected String device_info;
-
-    public String getReturn_code() {
-        return return_code;
-    }
-
-    public void setReturn_code(final String return_code) {
-        this.return_code = return_code;
-    }
-
-    public String getReturn_msg() {
-        return return_msg;
-    }
-
-    public void setReturn_msg(final String return_msg) {
-        this.return_msg = return_msg;
-    }
-
-    public String getAppid() {
-        return appid;
-    }
-
-    public void setAppid(final String appid) {
-        this.appid = appid;
-    }
-
-    public String getMch_id() {
-        return mch_id;
-    }
-
-    public void setMch_id(final String mch_id) {
-        this.mch_id = mch_id;
-    }
-
-    public String getDevice_info() {
-        return device_info;
-    }
-
-    public void setDevice_info(final String device_info) {
-        this.device_info = device_info;
-    }
-
-    public String getNonce_str() {
-        return nonce_str;
-    }
-
-    public void setNonce_str(final String nonce_str) {
-        this.nonce_str = nonce_str;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(final String sign) {
-        this.sign = sign;
-    }
-
-    public String getResult_code() {
-        return result_code;
-    }
-
-    public void setResult_code(final String result_code) {
-        this.result_code = result_code;
-    }
-
-    public String getErr_code() {
-        return err_code;
-    }
-
-    public void setErr_code(final String err_code) {
-        this.err_code = err_code;
-    }
-
-    public String getErr_code_des() {
-        return err_code_des;
-    }
-
-    public void setErr_code_des(final String err_code_des) {
-        this.err_code_des = err_code_des;
-    }
 }
