159cf45c2cca7fe11e5209e41ef793edb07a554e
ming.ma <isdom@qq.com>
2020-04-14 18:38:08 +0800

    返回类型再次修改为 Observable<ToWXMessage>

--- src/main/java/org/jocean/wechat/spi/msg/WXMessageResponder.java
+++ src/main/java/org/jocean/wechat/spi/msg/WXMessageResponder.java
@@ -1,8 +1,10 @@
 package org.jocean.wechat.spi.msg;
 
+import org.jocean.wechat.spi.ToWXMessage;
+
 import rx.Observable;
 
 // 该接口的实现类负责根据输入的微信消息，产生回复消息
 public interface WXMessageResponder extends WXMessageConsumer {
-    public Observable<Object> responseMessage(final BaseWXMessage msg);
+    public Observable<ToWXMessage> responseMessage(final BaseWXMessage msg);
 }
