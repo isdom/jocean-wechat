8e8be4492b22df369e38061f62b35b9ef4d80e53
ming.ma <isdom@qq.com>
2020-05-04 09:48:48 +0800

    add field detail for DefaultAuthorizedMP.toString()

--- src/main/java/org/jocean/wechat/service/DefaultAuthorizedMP.java
+++ src/main/java/org/jocean/wechat/service/DefaultAuthorizedMP.java
@@ -6,6 +6,16 @@ import org.springframework.beans.factory.annotation.Value;
 public class DefaultAuthorizedMP implements AuthorizedMP {
 
     @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("DefaultAuthorizedMP [componentAppid=").append(_componentAppid).append(", appid=")
+                .append(_appid).append(", token=").append(_token).append(", tokenExpireInMs=")
+                .append(_tokenExpireInMs).append(", refreshToken=").append(_refreshToken).append(", jsapiTicket=")
+                .append(_jsapiTicket).append("]");
+        return builder.toString();
+    }
+
+    @Override
     public String getComponentAppid() {
         return this._componentAppid;
     }
