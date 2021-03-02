c7ef23b8865a883fb73aa19104d435e70ab74f6e
ming.ma <isdom@qq.com>
2021-03-02 08:09:17 +0800

    upgrade to 0.0.2 for dep jocean-http to 0.0.3 and DecodeMsgUtil.decodeWXMessage using MessageUtil.cacheFullMessage

--- src/main/java/org/jocean/wechat/spi/msg/DecodeMsgUtil.java
+++ src/main/java/org/jocean/wechat/spi/msg/DecodeMsgUtil.java
@@ -1,23 +1,35 @@
 package org.jocean.wechat.spi.msg;
 
+import org.jocean.http.ContentDecoder;
 import org.jocean.http.ContentUtil;
-import org.jocean.svr.TradeContext;
+import org.jocean.http.FullMessage;
+import org.jocean.http.MessageUtil;
 
+import io.netty.handler.codec.http.HttpMessage;
+import io.netty.handler.codec.http.HttpRequest;
 import rx.Observable;
+import rx.functions.Actions;
 
 public class DecodeMsgUtil {
-    public static Observable<BaseWXMessage> decodeWXMessage(final TradeContext tctx) {
-        tctx.enableRepeatDecode();
-        return tctx.decodeBodyAs(ContentUtil.ASJSON, MsgType.class).flatMap(type -> {
+    public static Observable<BaseWXMessage> decodeWXMessage(final Observable<FullMessage<HttpRequest>> inbound) {
+        final Observable<FullMessage<HttpRequest>> cached = inbound.compose(MessageUtil.cacheFullMessage());
+        return decodeBodyAs(cached, ContentUtil.ASJSON, MsgType.class).flatMap(type -> {
             if (type.getType().equals("event")) {
-                return tctx.decodeBodyAs(ContentUtil.ASJSON, EnterSessionEvent.class);
+                return decodeBodyAs(cached, ContentUtil.ASJSON, EnterSessionEvent.class);
             } else if (type.getType().equals("text")) {
-                return tctx.decodeBodyAs(ContentUtil.ASJSON, TextWXMessage.class);
+                return decodeBodyAs(cached, ContentUtil.ASJSON, TextWXMessage.class);
             } else if (type.getType().equals("image")) {
-                return tctx.decodeBodyAs(ContentUtil.ASJSON, ImageWXMessage.class);
+                return decodeBodyAs(cached, ContentUtil.ASJSON, ImageWXMessage.class);
             } else {
                 return Observable.error(new RuntimeException("unknown wx message"));
             }
         });
+    }
+
+    private static <T, M extends HttpMessage> Observable<T> decodeBodyAs(
+            final Observable<FullMessage<M>> fhms,
+            final ContentDecoder decoder,
+            final Class<T> type) {
+        return fhms.flatMap(MessageUtil.fullmsg2body()).compose(MessageUtil.body2bean(decoder, type, Actions.empty()));
     }
 }
