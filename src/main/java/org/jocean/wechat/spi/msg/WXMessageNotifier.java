d4c7b6f09b5e41cb61f1b2f89ca5a5d8d40fcd0a
ming.ma <isdom@qq.com>
2021-03-03 10:13:48 +0800

    change WXMessageNotifier.notifyMessage return type

--- src/main/java/org/jocean/wechat/spi/msg/WXMessageNotifier.java
+++ src/main/java/org/jocean/wechat/spi/msg/WXMessageNotifier.java
@@ -1,11 +1,9 @@
 package org.jocean.wechat.spi.msg;
 
-import org.jocean.http.RpcRunner;
-
-import rx.Observable.Transformer;
+import rx.Observable;
 
 // 该接口的实现类根据输入的微信消息，产生一定动作，但并不回复消息给微信网关
 // 回复消息由微信接入服务自行决定，一般为 200 OK
 public interface WXMessageNotifier extends WXMessageConsumer {
-    public Transformer<RpcRunner, String> notifyMessage(final BaseWXMessage msg);
+    public Observable<String> notifyMessage(final BaseWXMessage msg);
 }
