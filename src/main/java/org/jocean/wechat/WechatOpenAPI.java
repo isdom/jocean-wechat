a7b7f7bbf98f0a8131f3ecb2860537fe39d90262
ming.ma <isdom@qq.com>
2021-06-22 18:49:02 +0800

    完成 WechatOpenAPI.fetchComponentToken 编写

--- src/main/java/org/jocean/wechat/WechatOpenAPI.java
+++ src/main/java/org/jocean/wechat/WechatOpenAPI.java
@@ -1,6 +1,14 @@
 package org.jocean.wechat;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jocean.rpc.annotation.OnResponse;
 import org.jocean.rpc.annotation.RpcBuilder;
+import org.jocean.wechat.spi.FetchComponentTokenRequest;
 import org.jocean.wechat.spi.FetchComponentTokenResponse;
 
 import rx.Observable;
@@ -8,6 +16,13 @@ import rx.Observable;
 public interface WechatOpenAPI {
     @RpcBuilder
     interface FetchComponentTokenBuilder {
+        @Produces(MediaType.APPLICATION_JSON)
+        FetchComponentTokenBuilder body(final FetchComponentTokenRequest req);
+
+        @POST
+        @Path("https://api.weixin.qq.com/cgi-bin/component/api_component_token")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @OnResponse("org.jocean.wechat.WXProtocol.CHECK_WXRESP")
         Observable<FetchComponentTokenResponse> call();
     }
 
--- src/main/java/org/jocean/wechat/spi/FetchComponentTokenResponse.java
+++ src/main/java/org/jocean/wechat/spi/FetchComponentTokenResponse.java
@@ -3,6 +3,8 @@
  */
 package org.jocean.wechat.spi;
 
+import org.jocean.wechat.WXProtocol.WXAPIResponse;
+
 import com.alibaba.fastjson.annotation.JSONField;
 
 
@@ -10,41 +12,17 @@ import com.alibaba.fastjson.annotation.JSONField;
  * @author isdom
  *
  */
-public class FetchComponentTokenResponse extends WechatResponse {
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append("FetchAccessTokenResponse [accessToken=")
-                .append(_accessToken).append(", expiresIn=").append(_expiresIn)
-                .append(", ").append(super.toString()).append("]");
-        return builder.toString();
-    }
+public interface FetchComponentTokenResponse extends WXAPIResponse {
 
     @JSONField(name="component_access_token")
-    public String getComponentToken() {
-        return _accessToken;
-    }
+    public String getComponentToken();
 
     @JSONField(name="component_access_token")
-    public void setComponentToken(final String accessToken) {
-        this._accessToken = accessToken;
-    }
+    public void setComponentToken(final String accessToken);
 
     @JSONField(name="expires_in")
-    public String getExpiresIn() {
-        return _expiresIn;
-    }
+    public String getExpiresIn();
 
     @JSONField(name="expires_in")
-    public void setExpiresIn(final String expiresIn) {
-        this._expiresIn = expiresIn;
-    }
-
-    private String _accessToken;
-
-    private String _expiresIn;
+    public void setExpiresIn(final String expiresIn);
 }
