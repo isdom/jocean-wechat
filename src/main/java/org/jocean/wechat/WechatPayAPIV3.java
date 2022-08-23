6c681fa05ee7bcf84353916de523d8db602dc31b
ming.ma <isdom@qq.com>
2022-08-23 20:49:05 +0800

    add WechatPayAPIV3

---
+++ src/main/java/org/jocean/wechat/WechatPayAPIV3.java
@@ -0,0 +1,27 @@
+package org.jocean.wechat;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.jocean.rpc.annotation.ConstParams;
+import org.jocean.rpc.annotation.OnBuild;
+import org.jocean.rpc.annotation.OnResponse;
+import org.jocean.rpc.annotation.RpcBuilder;
+import org.jocean.wechat.WXProtocol.Code2SessionResponse;
+import org.jocean.wechat.WXProtocol.OAuthAccessTokenResponse;
+import org.jocean.wechat.WXProtocol.WXAPIResponse;
+import org.jocean.wechat.spi.FetchComponentTokenResponse;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import rx.Observable;
+import rx.functions.Action2;
+
+public interface WechatPayAPIV3 {
+
+
+}
