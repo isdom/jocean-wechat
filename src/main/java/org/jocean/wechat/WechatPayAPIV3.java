197c449cd0a5d9a9370829f33c2784fca2c4831c
ming.ma <isdom@qq.com>
2022-08-28 22:23:35 +0800

    impl WechatPayAPIV3

--- src/main/java/org/jocean/wechat/WechatPayAPIV3.java
+++ src/main/java/org/jocean/wechat/WechatPayAPIV3.java
@@ -29,18 +29,7 @@ public interface WechatPayAPIV3 {
 	// https://pay.weixin.qq.com/wiki/doc/apiv3/open/pay/chapter4_3_3.shtml
 	// 商家转账到零钱开发指引
 	// https://api.mch.weixin.qq.com/v3/transfer/batches
-    public interface PreAuthCodeResponse extends WXAPIResponse {
-        @JSONField(name = "pre_auth_code")
-        public String getPreAuthCode();
-
-        @JSONField(name = "pre_auth_code")
-        public void setPreAuthCode(final String code);
-
-        @JSONField(name = "expires_in")
-        public int getExpires();
-
-        @JSONField(name = "expires_in")
-        public void setExpires(final int expires);
+    public interface V3TransferBatchesResponse extends WXAPIResponse {
     }
 
 
