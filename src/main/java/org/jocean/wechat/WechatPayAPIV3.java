6f77020e861ec802c89f586a3064f299563a00e2
ming.ma <isdom@qq.com>
2022-10-14 22:52:06 +0800

    add ref

--- src/main/java/org/jocean/wechat/WechatPayAPIV3.java
+++ src/main/java/org/jocean/wechat/WechatPayAPIV3.java
@@ -30,6 +30,7 @@ import rx.Observable.Transformer;
 //      https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay4_3.shtml
 //      https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay7_0.shtml
 //      https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay7_1.shtml
+//      https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay7_2.shtml
 //      https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay2_0.shtml
 //      https://pay.weixin.qq.com/wiki/doc/apiv3/wechatpay/wechatpay2_0.shtml#part-6
 
